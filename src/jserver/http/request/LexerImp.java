e1be58f502b486a3a7ed1b889f454f909ebbb894
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2018-10-16 17:25:56 +0200

    add lexer implementation
    Signed-off-by: Kamil Rusin <kamil.jakub.rusin@gmail.com>

---
+++ src/jserver/http/request/LexerImp.java
@@ -0,0 +1,45 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 nathiss.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package jserver.http.request;
+
+/**
+ * LexerImp for HTTP request.
+ * @author nathiss
+ */
+public class LexerImp implements Lexer {
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String [][] lex(String data) {
+    if (data == null)
+      return null;
+    String [][] ret = null;
+    String [] lines = data.split("[\\r\\n]+");
+    for (int i = 0; i < lines.length; i++) {
+      ret[i] = lines[i].split(" ");
+    }
+    return ret;
+  }
+}
