5d5d12415594dd3d80aabe1e8f3d5363f8e0bfa7
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2018-10-16 10:59:28 +0200

    add HTTP request interface
    Signed-off-by: Kamil Rusin <kamil.jakub.rusin@gmail.com>

---
+++ src/jserver/request/Request.java
@@ -0,0 +1,61 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 nathiss.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package jserver.request;
+
+/**
+ * Object representation of HTTP request.
+ * @author nathiss
+ */
+public interface Request {
+  /**
+   * Returns true if the request has been parsed properly, false otherwise.
+   * @return true if the request has been parsed properly, false otherwise
+   */
+  public boolean isValid();
+  
+  /**
+   * Returns a HTTP request method or null if the request is invalid.
+   * @return a HTTP request method or null if the request is invalid
+   */
+  public Method getMethod();
+  
+  /**
+   * Returns a URI or null if the request is invalid.
+   * @return a URI or null if the request is invalid
+   */
+  public String getURI();
+  
+  /**
+   * Returns a HTTP protocol version or null if the request is invalid.
+   * @return a HTTP protocol version or null if the request is invalid
+   */
+  public ProtocolVersion getProtocolVersion();
+  
+  /**
+   * Returns an associated value for a given key or null if key does not exist.
+   * @param key an index in the associative array
+   * @return a value associated with a given key or null if key does not exist
+   */
+  public String get(String key);
+}
