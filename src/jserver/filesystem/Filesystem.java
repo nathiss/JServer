4428761c5daaf97a0fda64589145779e89334989
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2018-10-17 15:09:33 +0200

    add root dir support and filesystem is now singleton
    Signed-off-by: Kamil Rusin <kamil.jakub.rusin@gmail.com>

--- src/jserver/filesystem/Filesystem.java
+++ src/jserver/filesystem/Filesystem.java
@@ -23,13 +23,23 @@
  */
 package jserver.filesystem;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Date;
 import jserver.http.HttpDate;
 
 /**
  * A filesystem.
  * @author nathiss
  */
-public interface Filesystem {
+public class Filesystem {
+  private Filesystem() {
+    this.rootDir = null;
+  }
+  
   /**
    * Returns true if a file under a given path exists and is a regular file,
    * false otherwise.
@@ -37,8 +47,10 @@ public interface Filesystem {
    * @return true if file under a given path exists and is a regular file, false
    * otherwise
    */
-  public boolean isRegularFile(String path);
-  
+  public boolean isRegularFile(String path) {
+    return new File(this.join(path)).isFile();
+  }
+
   /**
    * Returns true if a file under a given path exists and is a directory, false
    * otherwise.
@@ -46,27 +58,89 @@ public interface Filesystem {
    * @return true if file under a given path exists and is a directory, false
    * otherwise
    */
-  public boolean isDirectory(String path);
+  public boolean isDirectory(String path) {
+    return new File(this.join(path)).isDirectory();
+  }
   
   /**
    * Returns true if a file under a given path exists, false otherwise.
    * @param path
    * @return 
    */
-  public boolean exists(String path);
-  
+  public boolean exists(String path) {
+    return new File(this.join(path)).exists();
+  }
+
   /**
    * Reads a whole file under a given path and return its content or null if
    * the file can not be read. 
    * @param path a path to a file
-   * @return file content
+   * @return a file content
    */
-  public String read(String path);
-  
+  public String read(String path) {
+    try {
+      byte [] encoded = Files.readAllBytes(Paths.get(this.join(path)));
+      return new String(encoded, Charset.defaultCharset());
+    }
+    catch (IOException e) {
+      return null;
+    }
+  }
+
   /**
    * Returns a last modification date of a file under a given path.
    * @param path a path to a file
-   * @return last modification date
+   * @return a last modification date
    */
-  public HttpDate getModificationDate(String path);
+  public HttpDate getModificationDate(String path) {
+    long rawDate = new File(this.join(path)).lastModified();
+    return new HttpDate(new Date(rawDate));
+  }
+  
+  /**
+   * Returns a static instance of this class.
+   * @return a static instance
+   */
+  public static Filesystem getInstance() {
+    return instance;
+  }
+  
+  /**
+   * Sets a root directory for a filesystem.
+   * @param path a root directory path
+   */
+  public void setRootDir(String path) {
+    if (path.length() > 1 && path.charAt(path.length() - 1) == '/')
+      path = path.substring(0, path.length() - 1);
+    this.rootDir = path;
+  }
+  
+  /**
+   * Returns a root directory path.
+   * @return a root directory path
+   */
+  public String getRootDir() {
+    return this.rootDir;
+  }
+  
+  /**
+   * Concatenates two paths, path1 is a root.
+   * @param path1 a first path
+   * @param path2 a second path
+   * @return a concatenated path
+   */
+  private String join(String path) {
+    if (this.rootDir == null)
+      return path;
+    if (path.charAt(0) == '/')
+      path = path.substring(1);
+    return this.rootDir + path;
+  }
+ 
+  /**
+   * Singleton.
+   */
+  private static final Filesystem instance = new Filesystem();
+  
+  private String rootDir;
 }
