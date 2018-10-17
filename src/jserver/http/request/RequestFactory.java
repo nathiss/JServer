01aa575468366147e1b868882fb4951f4c12a374
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2018-10-17 13:13:26 +0200

    add request factory
    Signed-off-by: Kamil Rusin <kamil.jakub.rusin@gmail.com>

---
+++ src/jserver/http/request/RequestFactory.java
@@ -0,0 +1,48 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 nathiss.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package jserver.http.request;
+
+/**
+ * An abstract factory for a HTTP request object.
+ * @author nathiss
+ */
+public interface RequestFactory {
+  /**
+   * Creates a HTTP request lexer.
+   * @return a HTTP request lexer
+   */
+  public Lexer makeLexer();
+  
+  /**
+   * Creates a HTTP request parser.
+   * @return a HTTP request parser
+   */
+  public Parser makeParser();
+  
+  /**
+   * Creates a HTTP request.
+   * @return a HTTP request
+   */
+  public Request makeRequest();
+}
